68be2695be6bb57a2fe33696863169d5dedc85a6
Cameron Poe <cameron@zay.li>
2018-01-01 11:25:34 -0600

    zsh and vundle

--- coretools.sh
+++ coretools.sh
@@ -3,4 +3,14 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-apt-get install curl golang nodejs vim -y
+apt-get install curl golang nodejs vim zsh -y
+
+echo "Installing oh my zsh"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+echo "Installing Vim Vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+
+
+
