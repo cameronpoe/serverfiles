9d01ca195ad33c4109752c3bd1481668fc345969
Cameron Poe <cameron@zay.li>
2018-06-05 19:38:56 -0500

    shodanblock

---
+++ shodanblock.sh
@@ -0,0 +1,16 @@
+ufw deny from 93.120.27.62 to any
+ufw deny from 85.25.43.94 to any
+ufw deny from 85.25.103.50 to any
+ufw deny from 82.221.105.7 to any
+ufw deny from 82.221.105.6 to any
+ufw deny from 71.6.167.142 to any
+ufw deny from 71.6.165.200 to any
+ufw deny from 71.6.135.131 to any
+ufw deny from 66.240.236.119 to any
+ufw deny from 66.240.192.138 to any
+ufw deny from 198.20.99.130 to any
+ufw deny from 198.20.70.114 to any
+ufw deny from 198.20.69.98 to any
+ufw deny from 198.20.69.74 to any
+ufw deny from 188.138.9.50 to any
+ufw reload
